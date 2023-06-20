70ed71b43f8e6cbe766196a26dcb30505261137d
bluegause <53945115+bluegause@users.noreply.github.com>
2023-06-20 03:04:35 -1000

    misc

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -9,31 +9,47 @@ const NavBar = () => {
 
     const [isScrolled, setIsScrolled] = useState(false);
 
-        const handleScroll = () => {
-        const scrollTop = window.pageYOffset;
-        if (scrollTop > 50) {
-            setIsScrolled(true);
-        } else {
-            setIsScrolled(false);
-        }
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll)
+
+    }, []);
+
+    const handleScroll = () => {
+    const scrollTop = window.pageYOffset;
+    if (scrollTop > 50) {
+        setIsScrolled(true);
+    } else {
+        setIsScrolled(false);
+    }
+    };
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+        console.log('triggered');
     };
 
     return(
         <>
             <nav className={isScrolled ? "navbar scrolled" : "navbar"}>
-                <NavLink to="/"><img src={logo} alt="Maya Electrical logo" width={"45px"} height={"45px"}/></NavLink>
-                <div className="hamburger">
-                    <Hamburger direction="left" />
+                <img src={logo} alt="Maya Electrical logo" width={"45px"} height={"45px"}/>
+                <div className="hamburger" onClick={toggleMenu}>
+                    <Hamburger direction="left" toggled={isMenuOpen} toggle={setIsMenuOpen} />
                 </div>
-                <ul className="link-container">
-                    <li className="link">
+                <ul className={`link-container ${isMenuOpen ? 'is-open' : ''}`}>
+                    <li>
+                        <NavLink to="/" className="item">Home</NavLink>
+                    </li>
+                    <li>
                         <NavLink to="/services" className="item">Services</NavLink>
                     </li>
-                    <li className="link">
+                    <li>
                         <NavLink to="/about" className="item">About Us</NavLink>
                     </li>
                     <button className="contact-button">
-                        <NavLink to="/contact" className="item">Contact Us</NavLink>
+                        <NavLink to="/contact" className="contact-item">Contact Us</NavLink>
                     </button>
                 </ul>
                 
