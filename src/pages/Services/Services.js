320ca7b2f540c8153c69d84de9439b98a8e10f2d
bluegause <53945115+bluegause@users.noreply.github.com>
2023-08-01 07:09:09 -1000

    test

--- src/pages/Services/Services.js
+++ src/pages/Services/Services.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Grid } from '@mui/material';
 import { Servicesbg } from '../../assets/AssetsIndex';
+import { Transparentlogo } from '../../assets/AssetsIndex';
 import './services.css';
 
 const Services = () => {
@@ -40,45 +42,41 @@ const Services = () => {
                                 <p className='basic-typography'>
                                     We offer basic installation services for new and existing homes. For those who are wanting to take the first step by adding smart lighting capabilities, our experts have got you covered. This includes replacing your exisiting light switches with smart home capable ones, giving you the capabilities of controlling your lighting via smart phone, tablet, and even smart speaker devices. 
                                 </p>
+                                
                                 <div className='basic-reasons__container'>
-                                    <section>
-
-                                    
-                                    <div className='basic-card1'>
-                                        <h4 className='basic-card__header'>
-                                            Smart Lighting Switches
-                                        </h4>
-                                        <p className='basic-card__description'>
-                                            Switching your light switches to smart light switches allows you to have customizablity and control over your lighting brightness, as well as control over your lighting through your mobile & tablet devices. 
-                                        </p>
-                                    </div>
-                                    <div className='basic-card2'>
-                                        <h4 className='basic-card__header'>
-                                            System Programming
-                                        </h4>
-                                        <p className='basic-card__description'>
-                                            Switching your light switches to smart light switches allows you to have customizablity and control over your lighting brightness, as well as control over your lighting through your mobile & tablet devices. 
-                                        </p>
-                                    </div>
-                                    </section>
-                                    <section>
-                                    <div className='basic-card3'>
-                                        <h4 className='basic-card__header'>
-                                            Add Devices
-                                        </h4>
-                                        <p className='basic-card__description'>
-                                            Switching your light switches to smart light switches allows you to have customizablity and control over your lighting brightness, as well as control over your lighting through your mobile & tablet devices. 
-                                        </p>
-                                    </div>
-                                    <div className='basic-card4'>
-                                        <h4 className='basic-card__header'>
-                                            Programming Changes
-                                        </h4>
-                                        <p className='basic-card__description'>
-                                            Switching your light switches to smart light switches allows you to have customizablity and control over your lighting brightness, as well as control over your lighting through your mobile & tablet devices. 
-                                        </p>
-                                    </div>
-                                    </section>
+                                    <Grid container 
+                                        rowSpacing={1} 
+                                        columnSpacing={{xs: 1, sm: 2, md: 3}}
+                                        direction="row"
+                                        justifyContent="center"
+                                        alignItems="flex-start"
+                                        >
+                                        
+                                        <Grid item xs={4}>
+                                            <h4 className='basic-card__header'>
+                                                Smart Switches
+                                            </h4>
+                                            <p className='basic-card__description'>
+                                                Switching your light switches to smart light switches allows you to have customizablity and control over your lighting brightness and lighting schedule. We will replace all of your outdated switches with smart capable ones, which all can be controlled through a smart phone, tablet, smart speaker etc. Contact us to explore our switch options. 
+                                            </p>
+                                        </Grid>
+                                        <Grid item xs={4}>
+                                            <h4 className='basic-card__header'>
+                                                Programming Lighting
+                                            </h4>
+                                            <p className='basic-card__description'>
+                                                Program lighting to your personal liking. Create a schedule for when your lights turn on and off, explain how the controls work, and make sure you know how to operate every feature new to you. This includes what time your lights turn on/off, how bright the lighting is at certain times, and even what rooms are constantly lit. 
+                                            </p>
+                                        </Grid>
+                                        <Grid item xs={4}>
+                                            <h4 className='basic-card__header'>
+                                                Add Devices
+                                            </h4>
+                                            <p className='basic-card__description'>
+                                                Wanting to add more devices to your smart home? We offer services to current smart home owners as well. We can change older smart switches to new ones, add new switches to rooms that do not have them yet, reprogram them, and extras like motion sensors that turn on lights when passed by. 
+                                            </p>
+                                        </Grid>
+                                    </Grid>
                                 </div>
                             </div>
                         </div>
@@ -90,27 +88,78 @@ const Services = () => {
                                 <p className='works-typography'>
                                     Installing a panelized system will fully unlock your home's potential. We'll professionally install a control hub panel giving you control over all of your home's lighting at the touch of a finger tip. Installing a Panel in your home will replace your existing switches and dimmers from your wall, and replaces them with a customized keypad. Contact us to see if your home is eligible for a panelized system today. 
                                 </p>
-                                <div className='works-reasons__wrapper'>
-                                    <div className='works-reason1-wrapper'>
-                                        <h4 className='works-card__header'>
-
-                                        </h4>
-                                    </div>
-                                    <div className='works-reason2__wrapper'>
-                                        <h4 className='works-card__header'>
-                                            
-                                        </h4>
-                                    </div>
-                                    <div className='works-reason3__wrapper'>
-                                        <h4 className='works-card__header'>
-                                            
-                                        </h4>
-                                    </div>
+                                <div className='works-reasons__section'>
+                                    <Grid container
+                                        rowSpacing={1} 
+                                        columnSpacing={1}
+                                        direction="row"
+                                        justifyContent="center"
+                                        alignItems="flex-start"
+                                    >
+                                        <Grid item xs={4}>
+                                            <h4 className='works-card__header'>
+                                                Wiring
+                                            </h4>
+                                            <p className='works-card__description'>
+                                                Wiring your home will give you power and control all to your smart panel. We professionally route and install quality wires in your new or existing home without missing any details to ensure that you don't run into future problems or issues. Contact us to see if your home qualifies for a panelized system.
+                                            </p>
+                                        </Grid>
+                                        <Grid item xs={4}>
+                                            <h4 className='works-card__header'>
+                                                Panel System Install
+                                            </h4>
+                                            <p className='works-card__description'>
+                                                We offer different panel systems that you can choose from to suit your homes look. When installing a panel system, we will practically remove all your old switches/dimmers, and install a customized keypad in replacement. This allows control over all the lighting, give your home a modernized look, and escalating your lighting capabilities. 
+                                            </p>
+                                        </Grid>
+                                        <Grid item xs={4}>
+                                            <h4 className='works-card__header'>
+                                                Panel Programming
+                                            </h4>
+                                            <p className='works-card__description'>
+                                                Programming your panelized system is key. We will customize your lighting schedule and make sure that the keypad does exactly what you want. This includes lighting brightness, customized lighting for each room, and even when the lights turn on/off, all at a centralized location to maximize efficiency and accessibility. 
+                                            </p>
+                                        </Grid>
+                                    </Grid>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </section>
+                <div className='services-end' style={{backgroundImage: `url(${Transparentlogo})`}}>
+                    <div className='services-end__tint'>
+
+                    
+                    <h3 className='services-end__header'>
+                        Still have questions?
+                    </h3>
+                    <div className='services-end__container'>
+                        <div className='services-end__body'>
+                            <p className='services-end__typography'>
+                            Check out the FAQs page, or contact us and we'll answer any questions or concerns that you have regarding our services and how smart home installations can benefit you. Start today and experience the benefits of smart lighting<br/> - the Maya Electric Way.
+                            </p>
+                        </div>
+                        <div className='services-end__buttons'>
+                            <Link to='/faqs' className='service-button__link'>
+                                <button className='service-button__wrapped'>
+                                    FAQs
+                                </button>
+                            </Link>
+                            <Link to='/contact' className='service-button__link'>
+                                <button className='service-button__wrapped'>
+                                    Send a Message
+                                </button>
+                            </Link>
+                            <button className='service-button' href='tel:8583868083'> 
+                                Call Us
+                            </button>
+                            <button className='service-button' href='email:alberto@mayaelectrical.com'>
+                                Email Us
+                            </button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
             </div>  
         </>
     )
