b78a3e0ffa8a059a125fc5a4d2f4ef2c271c5382
bluegause <53945115+bluegause@users.noreply.github.com>
2023-10-04 13:22:35 -1000

    Lazy Load

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
+import React from 'react';
 import NavBar from "./components/NavBar/NavBar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
-import Services from "./pages/Services/Services";
-import About from "./pages/About/About";
-import Faqs from "./pages/Faqs/Faqs";
 import Contact from "./pages/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
 import "./App.css";
 
+const Faqs = React.lazy(() => import('./pages/Faqs/Faqs'));
+const About = React.lazy(() => import('./pages/About/About'));
+const Services = React.lazy(() => import('./pages/Services/Services'));
 function App() {
   return (
     <div className="app">
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/faqs" element={<Faqs />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/services"
+          element={ <React.Suspense fallback={<h1 style={{width: "100%",  color: "lightgray",fontSize: "4rem", paddingTop: "6rem",textAlign: "center", height: "100vh"}}>Loading...</h1>}>
+            <Services />
+          </React.Suspense>
+          }
+        />
+        <Route path="/about"
+          element={ <React.Suspense fallback={<h1 style={{width: "100%",  color: "lightgray",fontSize: "4rem", paddingTop: "6rem",textAlign: "center", height: "100vh"}}>Loading...</h1>}>
+            <About />
+          </React.Suspense>
+          }
+        />
+        <Route path="/faqs"
+          element={ <React.Suspense fallback={<h1 style={{width: "100%",  color: "lightgray",fontSize: "4rem", paddingTop: "6rem",textAlign: "center", height: "100vh"}}>Loading...</h1>}>
+            <Faqs />
+          </React.Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </div>
